741f5c9af8cf38000bedcd4f5b05c5f75dbf0fc4
Stephen <sdarcy29@hotmail.com>
2021-09-13 13:20:02 +0000

    Add two function to get random number for computer pick and the logic to pick a winner on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,60 @@ document.querySelector('.close--result').addEventListener("click", function() {
 
 // Variables for computers choice , result and scores
 
-let computerPicked = getComputerChoice();
+let computerPicked = computerPick();
 let result;
 let scores;
+
+
+/**
+ * Function to generate a random number to assign to computer game choice
+ */
+
+function computerPick() {
+
+    let computerPicked = Math.floor(Math.random() * 5);
+
+    if (computerPicked === 1) {
+        return 'rock';
+    } else if (computerPicked === 2) {
+        return 'paper'
+    } else if (computerPicked === 3) {
+        return 'scissors';
+    } else if (computerPicked === 4) {
+        return 'lizard';
+    } else if (computerPicked === 5) {
+        return 'spock';
+    } else {
+        return 'error'
+    }
+}
+
+/**
+ * This function decides the outcome and displays the result in a results modal
+ */
+
+
+function getTheResult(userPicked) {
+
+    if (userPicked === computerPicked) {
+        result = 'Its a draw, go again!';
+
+    } else {
+
+        if (userPicked === 'rock' && (computerPicked === 'scissors' || computerPicked === 'lizard')) {
+            result = 'You win';
+        } else if (userPicked === 'paper' && computerPicked === 'rock' || computerPicked === 'spock') {
+            result = 'You win';
+        } else if (userPicked === 'paper' && computerPicked === 'rock' || computerPicked === 'spock') {
+            result = 'You win';
+        } else if (userPicked === 'scissors' && computerPicked === 'paper' || computerPicked === 'lizard') {
+            result = 'You win';
+        } else if (userPicked === 'lizard' && computerPicked === 'spock' || computerPicked === 'paper') {
+            result = 'You win';
+        } else if (userPicked === 'spock' && computerPicked === 'scissors' || computerPicked === 'rock') {
+            result = 'You win';
+        } else {
+            result = 'You lose!'
+        }
+    }
+}
